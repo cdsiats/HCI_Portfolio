162507a1d55da8b0f63dfc69496afea075a8ebde
cdsiats <118641404+cdsiats@users.noreply.github.com>
2023-11-21 16:10:44 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+  let typed = new Typed(".typing", {
+    strings: ["Student", "Developer"],
+    typeSpeed: 100,
+    backSpeed: 50,
+    loop: true,
+    loopCount: Infinity,
+  });
+});
